154f3eb773f8629c3690928c4a03a5725e227274
gkevlin <g_kevlin@yahoo.com>
2017-04-21 23:41:45 -0700

    Working Config when password passed as parameter

--- get_run_cfg.py
+++ get_run_cfg.py
@@ -1,15 +1,36 @@
 from napalm import get_network_driver
+import sys, json
 
-driver = get_network_driver('ios')
+if len(sys.argv) < 2:
+    print('Usage: get_run_cfg.py PASSWORD')
+    exit()
 
-device = driver('172.16.1.80', 'cisco', 'cisco')
+with open('device_list.json') as device_list:
+    all_devices = json.load(device_list)
 
-device.open()
+for device in all_devices:
+    print ('Connecting to : ' + device['ip'])
+    driver = get_network_driver(str(device['driver']))
+    session = driver(str(device['ip']), str(device['username']), sys.argv[1])
+    session.open()
+    
+    fileString = '/git/switch_configs/' + device['platform'] + '/' + str(device['hostname']) + '.txt'
+    print('Writing config to : ' + fileString)
+    configFile = open(fileString, 'w')
 
-config = device.cli(commands=['show run'])
+    deviceConfig = session.get_config()
+    strDeviceConfig = deviceConfig['running']
+    configFile.write('\n'.join(strDeviceConfig.split('\n')[2:]))
 
-for x in config:
-	print (config[x])
+    configFile.close()
 
 
-device.close()
+#device.open()
+
+#config = device.cli(commands=['show run'])
+
+#for x in config:
+#	print (config[x])
+
+
+#device.close()
