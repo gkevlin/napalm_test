2d48e522847652e2d66614f897f236835721d9e2
gkevlin <g_kevlin@yahoo.com>
2017-04-27 22:51:05 -0700

    Change 1

--- get_run_cfg.py
+++ get_run_cfg.py
@@ -39,5 +39,5 @@ for device in all_devices:
                     configFile = open(fileString, 'w')
                     configFile.write('\n'.join(configText.split('\n')[2:]))
                     configFile.close()
-    except NetMikoAuthenticationException:
-        print('Incorrect Username/Password')
+    except:
+        print('Error connecting to ' + device)
