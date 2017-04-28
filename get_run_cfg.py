bf081966cefbc1b624b8ac2eb7392343bd98e971
gkevlin <g_kevlin@yahoo.com>
2017-04-27 22:33:17 -0700

    Change 1

--- get_run_cfg.py
+++ get_run_cfg.py
@@ -39,5 +39,5 @@ for device in all_devices:
                     configFile = open(fileString, 'w')
                     configFile.write('\n'.join(configText.split('\n')[2:]))
                     configFile.close()
-    except socket.error:
+    except AttributeError:
         print('Socket Error')
