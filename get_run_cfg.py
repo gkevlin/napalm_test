5e189c9e7ad0799844414936f0591dcac5048a40
gkevlin <g_kevlin@yahoo.com>
2017-04-27 22:40:38 -0700

    Change 1

--- get_run_cfg.py
+++ get_run_cfg.py
@@ -39,5 +39,5 @@ for device in all_devices:
                     configFile = open(fileString, 'w')
                     configFile.write('\n'.join(configText.split('\n')[2:]))
                     configFile.close()
-    except netmiko.ssh_exception.NetMikoAuthenticationException:
+    except napalm.get_network_driver():
         print('Incorrect Username/Password')
