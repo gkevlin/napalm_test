3678a8bfad7992d83d10eabcf2ebaad2ec246d24
gkevlin <g_kevlin@yahoo.com>
2017-04-27 22:28:12 -0700

    Change 1

--- get_run_cfg.py
+++ get_run_cfg.py
@@ -17,24 +17,27 @@ password = getpass("Enter Password : ")
 print(username + '   ' + password)
 
 for device in all_devices:
-    print ('Connecting to : ' + device['ip'])
-    #driver created by NAPALM to determine command structure and expects
-    driver = get_network_driver(str(device['driver']))
-    #session opens device with driver(ip/hostname, username, password)
-    with driver(str(device['ip']), username, password) as session:
-        # Gets start, running, and candidate(if applicable) for device
-        deviceConfig = session.get_config()
-        #export each config to a file in a local git path
+    try:
+        print ('Connecting to : ' + device['ip'])
+        #driver created by NAPALM to determine command structure and expects
+        driver = get_network_driver(str(device['driver']))
+        #session opens device with driver(ip/hostname, username, password)
+        with driver(str(device['ip']), username, password) as session:
+            # Gets start, running, and candidate(if applicable) for device
+            deviceConfig = session.get_config()
+            #export each config to a file in a local git path
 
-        for configType, configText in deviceConfig.iteritems():
-            #specifies the file path that the running config will be written to
-            if configText:
-                filePath = '/git/config_backup/' + device['site'] + '/' + device['platform'] + '/' + str(device['hostname']) + '/'
-                fileName = str(configType)
-                fileString = filePath + fileName
-                if not os.path.exists(filePath):
-                    os.makedirs(filePath)
-                print('Writing ' + str(configType) + 'config to : ' + filePath + fileName)
-                configFile = open(fileString, 'w')
-                configFile.write('\n'.join(configText.split('\n')[2:]))
-                configFile.close()
+            for configType, configText in deviceConfig.iteritems():
+                #specifies the file path that the running config will be written to
+                if configText:
+                    filePath = '/git/config_backup/' + device['site'] + '/' + device['platform'] + '/' + str(device['hostname']) + '/'
+                    fileName = str(configType)
+                    fileString = filePath + fileName
+                    if not os.path.exists(filePath):
+                        os.makedirs(filePath)
+                    print('Writing ' + str(configType) + 'config to : ' + filePath + fileName)
+                    configFile = open(fileString, 'w')
+                    configFile.write('\n'.join(configText.split('\n')[2:]))
+                    configFile.close()
+    except socket.error:
+        print('Error closing socket')
